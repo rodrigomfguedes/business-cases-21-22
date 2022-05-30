190f841c88d85ccf9d8f596999e63750fad0aa91
Rodrigo <79021594+rodrigomfguedes@users.noreply.github.com>
2022-05-30 23:03:54 +0100

    updated pip

---
+++ config.py
@@ -0,0 +1,35 @@
+TWITTER_CONSUMER_KEY = '7GhNnj3zvhKqrLbJGYF9uz5Uv'
+TWITTER_CONSUME_SECRET = 'sTH2ltOz0DmNi50bBelIwG8KkJTnwiHETKMDnwMFm0nxloUCSo'
+TWITTER_ACCESS_TOKEN = '1381626399455338503-E0p48e9CylnzXlslNqyUZQeBvUktT2'
+TWITTER_ACCESS_TOKEN_SECRET = 'TuY2Ys559MpGtQWIg4Zbz3A4lK5hyC3DBrLbhWgGUmsG0'
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader',
+    'PeterSchiff',
+    'Burns277',
+    'investorslive',
+    'markflowchatter',
+    'OptionsHawk',
+    'RedDogT3',
+    'tradertvneal',
+    'tradertvshawn',
+    'tradertvbrendan',
+    'ritholtz',
+    'jimcramer',
+    'PeterLBrandt',
+    'elerianm',
+    'ZeroHedge',
+    'paulkrugman',
+    'steve_hanke',
+    'TheStalwart',
+    'alaidi',
+]
+
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'postgres'
+DB_PASS = 'password'
+DB_NAME = 'etfdb'
